36cd78e3d0146a0df57fd8078e75c596101fdccd
senseihx4 <senseihx4@users.noreply.github.com>
2026-03-23 10:22:26 +0530

    testpush

--- fairy_club/login/urls.py
+++ fairy_club/login/urls.py
@@ -20,4 +20,9 @@ urlpatterns = [
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('manage_subscription/', views.manage_subscription, name='manage_subscription'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login_user'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html') , name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html') , name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset_your_password.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
 ]
