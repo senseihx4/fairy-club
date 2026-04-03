0b5e179524bf83fabdf6a0e4f08bf71a9bad19b6
senseihx4 <senseihx4@users.noreply.github.com>
2026-04-03 12:41:21 +0530

    Prepare for Railway deployment

--- fairy_club/fairy_club/settings.py
+++ fairy_club/fairy_club/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(m2pd2fwobcqln#o9gc(c3-2wv(1gugl0ufpvp#gz4a&ow^3^l'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-(m2pd2fwobcqln#o9gc(c3-2wv(1gugl0ufpvp#gz4a&ow^3^l")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
